ffceeb39b17f59f9bd0e4cb6ddb2d13f35eeb543
pavel_shapenov <pavel@144mln.com>
2015-07-07 16:03:38 +0500

    пагинация django-endless-pagination

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^company/create', views.company_create)
+    url(r'^company/create', views.company_create),
+    url(r'^card', views.CardIndex.as_view()),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from .forms import CompanyForm
 from django.http import HttpResponseRedirect
+from endless_pagination.views import AjaxListView
 
 # Create your views here.
+from polls.models import Card
+
 
 def company_create(request):
 	if request.method == 'POST':
@@ -12,4 +16,15 @@ def company_create(request):
 			return HttpResponseRedirect('/')
 	else:
 		form = CompanyForm()
-	return render(request, 'company/create.html', {'form' : form})
+	return render(request, 'company/create.html', {'form': form})
+
+
+class CardIndex(AjaxListView):
+	model = Card
+	template_name = 'card/card_index.html'
+	page_template = "card/card_list_page.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(CardIndex, self).get_context_data(**kwargs)
+		context['list'] = "card/card_list_page.html"
+		return context
